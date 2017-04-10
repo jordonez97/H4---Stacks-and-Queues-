4ca4771e70687553041f8cbde5a755d885602a19
JOSEPH-PC\Joseph <jsphordonez97@gmail.com>
2017-04-09 23:23:03 -0400

    Created Variables for the Queue class.

---
+++ Queue.java
@@ -0,0 +1,14 @@
+/* 
+ * Joseph Ordonez 
+ * H4 - Stack and Queue 
+ * Lab Section: M 10:25a - 11:25a 
+ * */
+
+public class Queue {
+  
+//private Node class
+  class Node {
+       //variables
+       String value; 
+       Node next; 
+  }}
