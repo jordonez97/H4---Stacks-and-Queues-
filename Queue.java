07544818004422b1cb30314039f82a4d2d6de81c
JOSEPH-PC\Joseph <jsphordonez97@gmail.com>
2017-04-09 23:27:03 -0400

    Created boolean isEmpty method for Queue. Initiated the size of the queue and returned it.

--- Queue.java
+++ Queue.java
@@ -22,9 +22,26 @@ public class Queue {
            this(val, null); 
        }  
    }
-  //initializes Node front and rear to null 
+    //initializes Node front and rear to null 
     Node front = null; 
     Node rear = null; 
+    
+    //boolean method isEmpty
+    public boolean isEmpty() {
+       return front == null; 
+    }
+  
+   //gets Length of queue
+   public int getLength(){
+       int size = 0; 
+       Node lengthNode = front;
+      
+       while(lengthNode != null) {
+           size++;
+           lengthNode = lengthNode.next;
+       }
+  return size; 
+   }
 }
 
   
